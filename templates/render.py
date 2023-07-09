7006c52e860f1342247d330e483c311828ef5cca
Ma_Victoria <victoriacursos2022@gmail.com>
2023-07-08 23:04:04 -0300

    templates

--- templates/render.py
+++ templates/render.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
+# Ruta de ejemplo
 @app.route('/')
 def index():
     return render_template('index.html')
